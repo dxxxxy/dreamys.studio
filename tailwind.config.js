c428d1b832d304567cfe5a71408bb0edd96eab55
DxxxxY <introsdream@gmail.com>
2021-06-03 18:40:34 -0400

    Change .gitgnore

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [
+        "*.html",
+    ],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem"
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
