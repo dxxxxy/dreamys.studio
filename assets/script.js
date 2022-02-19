b92a2017cad6bea3420b1db8d8352a8ebd0caa45
DxxxxY <introsdream@gmail.com>
2022-02-18 20:51:33 -0500

    new look, reorganised files (archive)

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,22 @@
-//particle.js
-particlesJS.load("particles-js", "assets/particles.json", () => console.log("particle.js config succesfully loaded"))
+//prevent right click
+document.addEventListener("contextmenu", event => event.preventDefault());
+
+//bg canvas
+VANTA.WAVES({
+    el: ".bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3a3ab3,
+    waveSpeed: 0.50,
+    zoom: 0.65
+})
 
 //links
-document.addEventListener("contextmenu", event => event.preventDefault());
 document.getElementById("github").addEventListener("click", () => window.location.href = "https://github.com/DxxxxY")
 document.getElementById("youtube").addEventListener("click", () => window.location.href = "https://www.youtube.com/channel/UCXLcxUX7GicuOisj8wZ8p2Q")
 document.getElementById("discord").addEventListener("click", () => alert("dxxxxy#0776"))
@@ -12,6 +26,6 @@ document.getElementById("website").addEventListener("click", () => window.locati
 document.querySelectorAll("img").forEach(e => {
     e.src = `assets/logos/${e.id}.png`
     e.alt = e.id
-    e.addEventListener("mouseover", () => e.style.filter = "invert(1)")
-    e.addEventListener("mouseout", () => e.style.filter = "invert(0)")
+    e.addEventListener("mouseover", () => e.style.filter = "invert(0)")
+    e.addEventListener("mouseout", () => e.style.filter = "invert(1)")
 })
