8c5360000deda191bf0a689f9166cbd2286e3512
dxxxxy <introsdream@gmail.com>
2023-06-13 13:42:48 -0400

    feat: setup and particles

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { loadFull } from "tsparticles";
+  import PARTICLES from "./assets/particles.json"
+
+  const particlesInit = async engine => {
+      await loadFull(engine);
+  };
+
+  const particlesLoaded = async container => {
+      console.log("Particles container loaded", container);
+  };
+</script>
+
+<template>
+  <div class="particles"><Particles id="tsparticles" :particlesInit="particlesInit" :particlesLoaded="particlesLoaded" :options="PARTICLES"/></div>
+</template>
+
+<style>
+    div.particles {
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        z-index: 0;
+        filter: blur(10px) invert(1) hue-rotate(235deg);
+    }
+    </style>
